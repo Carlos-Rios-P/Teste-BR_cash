2ca30e576b7cdcf1920972c94e204ec1a90f3883
Carlos Rios <carlosriospontes@gmail.com>
2022-06-10 12:44:38 -0300

    fix erro

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Card;
 use App\Models\Transaction;
-use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
