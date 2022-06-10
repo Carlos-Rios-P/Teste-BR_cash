3f42e0d29f0a9c7ed962b59dd2221104fe46ddc6
Carlos Rios <carlosriospontes@gmail.com>
2022-06-10 12:27:48 -0300

    implementing models Card and Transaction

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'card_number',
+        'card_expiration_date',
+        'card_holder_name',
+        'card_cvv',
+        'transaction_id',
+        'card_first_digits',
+        'card_last_digits',
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
 }
